901c09935696030d8c3270c2f3005c58496c9dc2
Nicholas <131720762+NicholasGuize@users.noreply.github.com>
2023-11-27 20:27:58 -0300

    Add files via upload
    update 27/11/2023

---
+++ EasyPark-main 2.0/eparkproject/eparkweb/src/test/java/com/epark/eparkweb/EparkwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epark.eparkweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EparkwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
