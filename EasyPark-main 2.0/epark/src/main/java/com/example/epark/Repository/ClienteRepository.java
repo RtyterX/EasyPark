1f3d82869c816321d32c50989ee633def955ba11
Ricardo Thiago Firmino <129962383+RtyterX@users.noreply.github.com>
2023-11-27 21:30:55 -0300

    Delete EasyPark-main 2.0 directory

--- EasyPark-main 2.0/epark/src/main/java/com/example/epark/Repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.epark.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.epark.Entity.Cliente;
-
-public interface ClienteRepository extends JpaRepository< Cliente, Long  > {
-
-}
